0dd5488c7729d9dfceb9f6cc09eb3918eb967b17
AnnieAmrod <mirianduran@alu.easda.es>
2023-05-06 21:22:48 +0200

    Plantillas Home e Inicio terminadas con diseño

--- proyectoKAT/common/views.py
+++ proyectoKAT/common/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'common/home.html'
+
+class PanelView(TemplateView):
+    template_name = 'common/panel_inicio.html'
--- proyectoKAT/proyectoKAT/urls.py
+++ proyectoKAT/proyectoKAT/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from common import views
 
 urlpatterns = [
 path('admin/', admin.site.urls),
+path('', views.HomeView.as_view(), name='home'),
+path('home/', views.HomeView.as_view(), name='home'),
+path('panel/', views.PanelView.as_view(), name='panel'),
 ]
 
 if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
